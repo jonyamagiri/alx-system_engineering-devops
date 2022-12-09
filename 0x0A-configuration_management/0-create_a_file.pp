4bd9c08e2d2c8b046a5b8355f7358b3c09e903fe
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-12-09 20:38:38 +0300

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,9 +2,9 @@
 
 file { '/tmp/school':
   ensure  => file,
-  path    =>  '/tmp/school',
-  mode    =>  '0744',
-  owner   =>  'www-data',
-  group   =>  'www-data',
-  content =>  'I love puppet'
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }

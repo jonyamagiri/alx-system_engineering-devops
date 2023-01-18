b039a258a2a5b52a82f5324dc3ea5b0cff2d956f
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-01-18 22:28:53 +0300

    fix web_server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,19 +1,23 @@
 # Puppet manifest that installs and configures Nginx web server
 
+# install Nginx
 package { 'nginx':
   ensure => installed,
 }
 
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'file',
-  content => "listen 80 default_server;\nrewrite ^/redirect_me https://www.youtube.com permanent;",
-}
-
+# configure welcome page
 file { '/var/www/html/index.html':
   ensure  => 'file',
   content => 'Hello World!',
 }
 
+# configure redirection
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  content => "listen 80 default_server;\nrewrite ^/redirect_me https://www.youtube.com permanent;",
+}
+
+# Nginx restart
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],

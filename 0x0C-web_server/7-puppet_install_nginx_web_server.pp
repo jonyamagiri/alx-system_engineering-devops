6f698f4e98896d51c4f94c16a1e8b73101c4fac5
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-01-18 22:25:02 +0300

    fix web_server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,13 +1,20 @@
-# Install and configure Nginx web server with Puppet
+# Puppet manifest that installs and configures Nginx web server
 
-exec { 'server configuration':
-  provider => shell,
-  command  => [
-    'sudo apt-get -y update',
-    'sudo apt-get -y install nginx',
-    'echo "Hello World!" > /var/www/html/index.html',
-    'sudo sed -i "/server_name _;/a location /redirect_me {\n\treturn 301 https://google.com;\n\t}\n"',
-    '/etc/nginx/sites-available/default',
-    'sudo service nginx restart',
-  ],
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'file',
+  content => "listen 80 default_server;\nrewrite ^/redirect_me https://www.youtube.com permanent;",
+}
+
+file { '/var/www/html/index.html':
+  ensure  => 'file',
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }

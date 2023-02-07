6690824dfa20cbba7d5098111744f93980bfd704
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-02-07 08:18:25 +0000

    api_advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Contains the number_of_subscribers function
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': 'myBot/0.0.1'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs

a2230f3dc55af87fb9bb53f2d9effe75508a4326
Joab Nyamagiri <jonyamagiri@gmail.com>
2023-03-08 07:31:28 +0300

    web_stack_debugging_3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Puppet manifest to fix wordpress settings on site running on apache2
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
